de7283748dc6b1b6d85f06b27d67c1c0dc98fa4d
MickeyParkman <wrestlingparkman@gmail.com>
2014-12-14 03:13:43 -0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Game.java

--- Start.java
+++ Start.java
@@ -1,7 +1,19 @@
+import javax.swing.JFrame;
+
 public class Start extends JFrame
 {
+
+   public Start()
+   {
+      setContentPane(new Game());
+      pack();
+      setLocationRelativeTo(null);
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setVisible(true);
+   }
+
    public static void main(String[] args)
    {
-      System.out.println("Hello World");
+      new Start();
    }
 }
