edd2dd65c9d6bb5530e935ac87c9b50d351c5dae
MickeyParkman <wrestlingparkman@gmail.com>
2014-12-14 01:24:08 -0800

    F*** You Gordan

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
     Serial* SP = new Serial("\\\\.\\COM3");    // adjust as needed
 
 	if (SP->IsConnected())
 		printf("We're connected");
+	else
+		std::cout << "You are a fucking retard and this port does not exist. IE: FUCK YOU GORDAN" << std::endl;
     return 0;
 
 	
